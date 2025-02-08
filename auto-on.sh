d9a6a3c6e87d8bd1bb581731744823794eaa2157
juancana <89663031+juancana@users.noreply.github.com>
2025-02-08 01:10:31 +0100

    Create auto-on.sh

---
+++ auto-on.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+
+# Default variable values
+apagado=false
+minutos=0
+horas=0
+confirmation=true
+
+# Function to display script usage
+usage() {
+ echo "Usage: $0 [OPTIONS]"
+ echo "Options:"
+ echo " -h	Display this help message"
+ echo " -a 	Shutdown system after programming wakealarm"
+ echo " -t	Time in hours"
+ echo " -m 	Time in minutes"
+ echo " -y	Do not ask for confirmation"
+ echo ""
+ echo "  Example:"
+ echo "  	$0 -a -t 1 -m 30	Shutdown and wake in 1h30m"
+ echo "  	$0 -t 5  		Wake in 5h"
+ echo ""
+ echo ""
+ echo ""
+ echo "  Automate with crontab:"
+ echo ""
+ echo "		sudo cp auto-on.sh /root/bin/"
+ echo "		sudo crontab -e"
+ echo ""
+ echo "		<Customize and add line/s>"
+ echo "		30 23 * * 1-5 /root/bin/auto-on.sh -a -t 8 -m 30 -y"
+ echo "		0 2 * * 0,6 /root/bin/auto-on.sh -a -t 7 - y"
+ echo ""
+ echo ""
+ echo ""
+}
+
+
+# Handle options and arguments
+while getopts 'hat:m:y' OPTION; do
+  case "$OPTION" in
+    h)
+     usage
+     exit 0
+     ;;
+    a)
+     apagado=true
+     ;;
+    t)
+     horas="$OPTARG"
+     re='^[0-9]+$'
+     if ! [[ $horas =~ $re ]] ; then
+      echo "ERROR Invalid number of hours"
+      echo""
+      exit 1
+     fi
+     retraso=0
+     ;;
+    m)
+     minutos="$OPTARG"
+     re='^[0-9]+$'
+     if ! [[ $minutos =~ $re ]] ; then
+      echo "ERROR Invalid number of minutes"
+      echo""
+      exit 1
+     fi
+     retraso=0
+     ;;
+    y)
+     confirmation=false
+     ;;
+    ?)
+     echo "Invalid option."
+     echo "Use $0 -h to display help menu"
+     echo ""
+     exit 1
+     ;;
+  esac
+done
+
+
+
+# Main script execution
+
+# Calcular hora de encendido
+if [[ -z $retraso ]]; then
+ retraso=480
+ echo ""
+ echo "Programming power on in 8h"
+else
+ retraso=$(($minutos+($horas*60)))
+ echo ""
+ echo "Programming power on in "$horas"h "$minutos"m"
+fi
+
+
+# Confirmar con usuario
+if [ "$confirmation" = "true" ]; then
+ if [ "$apagado" = true ]; then
+  echo "  and shut down now"
+ else
+  echo "  but not shutdown"
+ fi
+
+ echo ""
+ printf "%s " "Continue (y/N)? "
+ read ans
+ if [[ "$ans" != "y" ]]; then
+  echo ""
+  echo "Quitting..."
+  echo ""
+  exit 0
+ fi
+fi
+
+# Programar encendido automatico dentro de X horas
+parte1="echo \`date '+%s' -d '"
+parte2="+ "$(printf "%d" $retraso)" minutes"
+comando=$parte1$parte2"'\` > /sys/class/rtc/rtc0/wakealarm"
+sh -c "$comando"
+
+if [ "$apagado" = true ]; then
+ echo ""
+ echo "Apagando equipo..."
+ echo ""
+ shutdown -h now
+fi
